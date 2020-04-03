8a6192fa1fb6226d1b7874717635d7dd21da319d
Haeyoon <haeyoon.devops@gmail.com>
2020-04-03 21:27:24 +0300

    commit

--- python_test/python_check_mariadb_version.py
+++ python_test/python_check_mariadb_version.py
@@ -4,7 +4,8 @@ from urllib.request import urlopen
 
 db = pymysql.connect(host='db', port='3306', user='haeyoon', passwd='1234', db='example_db', charset='utf8', autocommit=True)
 cursor = db.cursor()
-cursor.execute("select version()")
-data = cursor.fetchone()
-print("------------------- db version: %s " % data)
+#cursor.execute("select version()")
+cursor.execute("insert into example_db (id, INS_DATE, NAME, VALUE) values(11, now(), 'example-11', 'value-11');")
+#data = cursor.fetchone()
+#print("------------------- db version: %s " % data)
 db.close()
